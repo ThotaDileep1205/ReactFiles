fba584b758f77b83538702a3251a9533b3c13d6c
ThotaDileep1205 <133356612+ThotaDileep1205@users.noreply.github.com>
2023-06-13 07:50:32 +0530

    Initial commit

---
+++ src/list.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+function List() {
+  return (
+    <ul>
+      <li>Good Morinig</li>
+      <li>Good Afternoon</li>
+      <li>Good Evening</li>
+    </ul>
+  )
+}
+export default List
